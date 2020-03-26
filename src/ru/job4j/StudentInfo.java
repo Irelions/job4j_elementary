54e5e0f28c898cc14a0f57c056afab895fdc2b96
Irelion <irelion@mail.ru>
2020-03-26 22:09:54 +0300

    3.3. Разбор первой программы.[#262246]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Evgeniy Ivanov");
+        System.out.println("11.12.1985");
+
+    }
+}
