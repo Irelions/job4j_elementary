56062834b5399d0c3339a7fd69a5091ccff5f740
Irelion <irelion@mail.ru>
2020-03-27 22:52:36 +0300

    0. Что такое переменная.[#262240]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -2,6 +2,10 @@ package ru.job4j;
 
 public class Main {
     public static void main(String [] args) {
-        System.out.println("Hello world");
+
+        int age = 18;
+        float length =  78.0332f;
+        boolean baby = true;
+
     }
 }
