b52a98bd7b820f99ce029eced4d4017bfb78309c
Irelion <irelion@mail.ru>
2020-03-29 22:03:37 +0300

    3.3. Разбор первой программы.[#262246]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -2,10 +2,6 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-
-        int age = 18;
-        float length =  78.0332f;
-        boolean baby = true;
-
+        System.out.println("Hello world");
     }
 }
