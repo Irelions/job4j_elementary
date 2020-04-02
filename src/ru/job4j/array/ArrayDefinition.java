e4c985ae75a3eb63206948a78377b7811d046f02
Irelion <irelion@mail.ru>
2020-04-02 14:40:02 +0300

    6.0.2. Заполнение массива.[#262215]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -7,5 +7,18 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] price = new float[40];
+
+        String[] names = new String[4];
+
+        names[0] = "Agatha MacDonald";
+        names[1] = "William Jacobson";
+        names[2] = "Audrey Smith";
+        names[3] = "Theresa Holiday";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
